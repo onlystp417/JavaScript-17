89ec70f555146fa9c065a8c82dc3fa2b73cf2865
JinWen <onlystp417@gmail.com>
2020-03-30 14:35:58 +0800

    build: unit test

---
+++ js01/_test_/js01.test.js
@@ -0,0 +1,33 @@
+const {
+  confirmInput,
+  getPlayableMessage
+} = require("../js/js01modules");
+
+describe('Question Interface', () => {
+  describe('過濾不合法的值', () => {
+    it('輸入 hello 要回 NaN 錯誤', () => {
+      expect(() => confirmInput('hello')).toThrow('呃...是要輸入人數哦！(╬☉д⊙)');
+    })
+    it('沒輸入值要回 " " 錯誤', () => {
+      //此處要用 callback 才可以，可是 call function 才能放進引數，所以用匿名 funtion 去包裹，讓它不要在外部執行，而是透過匿名 function 執行。
+      expect(() => confirmInput('')).toThrow('要輸入人數!!!!!!');
+      // try {
+      //   confirmInput('')
+      // } catch (error) {
+      //   expect(error.message).toMatch('要輸入人數!!!!!!');
+      // }
+    })
+    it('輸入 -1 要回 <0 錯誤', () => {
+      expect(() => confirmInput(-1)).toThrow('呃...人數不會小於零！(╬☉д⊙)');
+    })
+  })
+  describe('分辨要播放或不播放', () => {
+    it('輸入 0 要回取消播放訊息', () => {
+      expect(getPlayableMessage(0)).toMatch('本場次沒有觀眾，取消播放。(◞‸◟)');
+    })
+    it('輸入 12 要回如期播放訊息', () => {
+      expect(getPlayableMessage(12)).toMatch(`進場人數為 12 人，祝觀影愉快。(灬ºωº灬)`);
+    })
+  })
+
+})
---
+++ js01/js/js01-argv.js
@@ -0,0 +1,30 @@
+// 1. 好想電影院規定除非完全沒有客人買票，否則就算只有一位顧客也照常播放電影。讓使用者輸入客人的人數，並用「!」判斷人數，不為零顯示「照常播放電影」。
+
+// 解構賦值 Destructuring Assignment
+const {
+  confirmInput,
+  getPlayableMessage
+} = require("./js01modules");
+
+var strTotalPurchaser = +process.argv[2];
+
+console.log(`好想電影院本場次買票人數： ${strTotalPurchaser} 人。`);
+
+catchValues(strTotalPurchaser);
+
+function catchValues(strTotalPurchaser) {
+  try {
+    const numTotalPurchaser = confirmInput(strTotalPurchaser);
+    const message = getPlayableMessage(numTotalPurchaser);
+    console.log(message);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+// if (Number.isNaN(strTotalPurchaser) || strTotalPurchaser < 0) {
+//   console.log('呃...是要輸入人數哦！(╬☉д⊙)');
+// } else if (strTotalPurchaser === 0) {
+//   console.log('本場次沒有觀眾，取消播放。(◞‸◟)');
+// } else {
+//   console.log(`進場人數為 ${strTotalPurchaser} 人，祝觀影愉快。(灬ºωº灬)`)
+// }
