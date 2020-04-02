bb351f577c634fbd7190d0be09c8bc92df4f71ed
JinWen <onlystp417@gmail.com>
2020-04-02 18:03:15 +0800

    feature: each input confirm

--- js02/js/js02.js
+++ js02/js/js02.js
@@ -1,35 +1,26 @@
 // 2. 使用者輸入兩個整數，將它們分別除以 3 ，判斷餘數是否相同，若相同，則於螢幕上顯示「餘數相同」。
 
-// 引入 node.js 的 readline module
-const readline = require('readline');
-
 // 引入 js module
 const {
   confirmInput,
-  compareTwoNumDivideThree
+  secondInputInterface
 } = require("./js02modules");
 
-// 透過 createInterface [class] 製造 readline 的問答介面
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// 引入寫在外層的 readline createInterface module，
+const rl = require("../../readline");
 
 // 輸入介面
 function inputInterface() {
-  rl.question('請輸入第一個整數', (strAanswer1) => {
-    rl.question('請輸入第二個整數', (strAnswer2) => {
-      try {
-        confirmInput(strAanswer1, strAnswer2);
-        const message = compareTwoNumDivideThree(strAanswer1, strAnswer2);
-        console.log(message);
-        rl.close();
-      } catch (error) {
-        console.log(error.message);
-        inputInterface();
-      }
-    })
+  rl.question('請輸入第一個整數：', (strFirstAanswer) => {
+    try {
+      confirmInput(strFirstAanswer);
+    } catch (error) {
+      console.log(error.message);
+      inputInterface();
+    }
+    secondInputInterface(strFirstAanswer);
   })
 }
 
 inputInterface();
+module.exports = inputInterface;
--- js02/js/js02modules.js
+++ js02/js/js02modules.js
@@ -1,25 +1,41 @@
 module.exports = {
   confirmInput,
-  compareTwoNumDivideThree
+  compareTwoNumDivideThree,
+  secondInputInterface
 }
 
-// 檔掉無效的值
-function confirmInput(strAnswer1, strAnswer2) {
-  let firstIntager = +strAnswer1;
-  let secondIntager = +strAnswer2;
-  if (Number.isNaN(firstIntager) || Number.isNaN(secondIntager)) {
+// 第二次提問
+const rl = require("../../readline");
+function secondInputInterface(strFirstAanswer) {
+  rl.question('請輸入第二個整數：', function (strSecondAnswer) {
+    try {
+      confirmInput(strSecondAnswer);
+      const message = compareTwoNumDivideThree(strFirstAanswer, strSecondAnswer);
+      console.log(message);
+      rl.close();
+    } catch (error) {
+      console.log(error.message);
+      secondInputInterface(strFirstAanswer);
+    }
+  })
+}
+
+// 過濾不合規定的值
+function confirmInput(strAnswer) {
+  let numAnswer = +strAnswer;
+  if (Number.isNaN(numAnswer)) {
     throw new Error('你所輸入的值含有非數字，請重新輸入：');
-  } else if (strAnswer1 === "" || strAnswer2 === "") {
+  } else if (!(strAnswer)) {
     throw new Error('你漏輸入值了，請重新輸入：');
-  } else if (firstIntager % 1 !== 0 || secondIntager % 1 !== 0) {
+  } else if (numAnswer % 1) {
     throw new Error('你輸入的值含有浮點數，請重新輸入：');
   }
 }
 
 // 分辨兩個整數除以三，於數是否相同
-function compareTwoNumDivideThree(strAanswer1, strAnswer2) {
-  let firstIntager = +strAanswer1;
-  let secondIntager = +strAnswer2;
+function compareTwoNumDivideThree(strFirstAanswer, strSecondAnswer) {
+  let firstIntager = +strFirstAanswer;
+  let secondIntager = +strSecondAnswer;
   if (firstIntager % 3 === secondIntager % 3) {
     return '餘數相同。';
   } else {
