4369fd406a6d181167c1876a141b2215fe189b32
JinWen <onlystp417@gmail.com>
2020-04-03 15:29:05 +0800

    fix: js02 test for each input confirm

--- js02/_test_/js02.test.js
+++ js02/_test_/js02.test.js
@@ -5,14 +5,14 @@ const {
 
 describe('檢查兩個整數除以三於數是否相等', () => {
   describe('confirmInput', () => {
-    it('輸入 "abc" 跟 2 要回 NaN 錯誤', () => {
-      expect(() => confirmInput('abc', 2)).toThrow('你所輸入的值含有非數字，請重新輸入：');
+    it('輸入 "abc" 要回 NaN 錯誤', () => {
+      expect(() => confirmInput('abc')).toThrow('你所輸入的值含有非數字，請重新輸入：');
     })
-    it('輸入 "" 跟 123 要回空字串錯誤', () => {
-      expect(() => confirmInput('', 123)).toThrow('你漏輸入值了，請重新輸入：')
+    it('輸入 "" 要回空字串錯誤', () => {
+      expect(() => confirmInput('')).toThrow('你漏輸入值了，請重新輸入：')
     })
-    it('輸入 0.2 跟 1 要回浮點數錯誤', () => {
-      expect(() => confirmInput(0.2, 1)).toThrow('你輸入的值含有浮點數，請重新輸入：')
+    it('輸入 0.2 要回浮點數錯誤', () => {
+      expect(() => confirmInput(0.2)).toThrow('你輸入的值含有浮點數，請重新輸入：')
     })
   })
   describe('compareTwoNumDivideThree', () => {
