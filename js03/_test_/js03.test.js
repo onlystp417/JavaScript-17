6ee25b8c5defa3b170c3af0269077b035dca61ba
JinWen <onlystp417@gmail.com>
2020-04-06 13:27:32 +0800

    fix: js03 boundary test

--- js03/_test_/js03.test.js
+++ js03/_test_/js03.test.js
@@ -18,25 +18,43 @@ describe('判斷消費者年齡並計算票價', () => {
     })
   })
   describe('ticketPriceCaculater', () => {
-    it('輸入 4 票價為 200 元', () => {
-      expect(ticketPriceCaculater(4)).toMatch('你可以買兒童票，票價為 200 元。');
+    it('輸入 5 票價為 200 元', () => {
+      expect(ticketPriceCaculater(5)).toMatch('你可以買兒童票，票價為 200 元。');
     })
-    it('輸入 75 票價為 200 元', () => {
-      expect(ticketPriceCaculater(75)).toMatch('你可以買敬老票，票價為 200 元。');
+    it('輸入 6 票價為 200 元', () => {
+      expect(ticketPriceCaculater(6)).toMatch('你可以買兒童票，票價為 200 元。');
     })
-    it('輸入 27 票價為 400 元', () => {
-      expect(ticketPriceCaculater(27)).toMatch('你要買的是普通票，票價為 400 元。');
+    it('輸入 7 票價為 200 元', () => {
+      expect(ticketPriceCaculater(7)).toMatch('你要買的是普通票，票價為 400 元。');
+    })
+    it('輸入 64 票價為 200 元', () => {
+      expect(ticketPriceCaculater(64)).toMatch('你要買的是普通票，票價為 400 元。');
+    })
+    it('輸入 65 票價為 200 元', () => {
+      expect(ticketPriceCaculater(65)).toMatch('你可以買敬老票，票價為 200 元。');
+    })
+    it('輸入 66 票價為 200 元', () => {
+      expect(ticketPriceCaculater(66)).toMatch('你可以買敬老票，票價為 200 元。');
     })
   })
   describe('ticketTypeClassify', () => {
-    it('輸入 2 票種為兒童票', () => {
-      expect(ticketTypeClassify(2)).toMatch('兒童票');
+    it('輸入 5 票種為兒童票', () => {
+      expect(ticketTypeClassify(5)).toMatch('兒童票');
     })
-    it('輸入 87 票種為敬老票', () => {
-      expect(ticketTypeClassify(87)).toMatch('敬老票');
+    it('輸入 6 票種為兒童票', () => {
+      expect(ticketTypeClassify(6)).toMatch('兒童票');
     })
-    it('輸入 48 票種為普通票', () => {
-      expect(ticketTypeClassify(48)).toMatch('普通票');
+    it('輸入 7 票種為兒童票', () => {
+      expect(ticketTypeClassify(7)).toMatch('普通票');
+    })
+    it('輸入 64 票種為敬老票', () => {
+      expect(ticketTypeClassify(64)).toMatch('普通票');
+    })
+    it('輸入 65 票種為敬老票', () => {
+      expect(ticketTypeClassify(65)).toMatch('敬老票');
+    })
+    it('輸入 66 票種為敬老票', () => {
+      expect(ticketTypeClassify(66)).toMatch('敬老票');
     })
   })
 })
